71ae38c9567864893c12846254c198d8dc352ca6
mfrankii <moshe.frankipour@intel.com>
2022-12-10 20:29:07 +0200

    add validate_password func

--- api/code/schemas/user.py
+++ api/code/schemas/user.py
@@ -25,14 +25,16 @@ def userEntity(item) -> dict:
 def hash(password: str) -> str:
     # Generate a random salt
     salt = str(random.getrandbits(128))
-
     # Use HMAC to hash the password with the salt
     password_hash = hmac.new(salt.encode(), password.encode(), hashlib.sha256).hexdigest()
-
     # Return the salt and the hash as one string
     return salt + password_hash
 
+def validate_password(password: str, password_hash: str) -> bool:
+    # Extract the salt from the password hash
+    salt = password_hash[:128]
+    # Use HMAC to hash the password with the salt and compare it to the stored password hash
+    return hmac.new(salt.encode(), password.encode(), hashlib.sha256).hexdigest() == password_hash[128:]
 
-# TODO: imp
 def usersEntity(entity) -> list:
     return [userEntity(item) for item in entity]
