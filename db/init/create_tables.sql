df7b31cf5919554b9706ef0f9eb6accb30ddd103
mfrankii <moshe.frankipour@intel.com>
2022-12-13 20:31:49 +0200

    fix size of password

--- db/init/create_tables.sql
+++ db/init/create_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `db`.`Users` (
     `user_id` INT NOT NULL AUTO_INCREMENT,
     `userName` VARCHAR(25) NOT NULL,
     `email` VARCHAR(45) NOT NULL,
-    `password` VARCHAR(45) NOT NULL,
+    `password` VARCHAR(125) NOT NULL,
     `plan_id` INT NOT NULL,
     CONSTRAINT user_pk PRIMARY KEY (`user_id`)
 );
